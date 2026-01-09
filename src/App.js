134ee5f9781a9bf65ca7f08bf6e105f310636fda
vivekmuthal-alt <vivekmutha@praxis360.in>
2026-01-09 12:43:38 +0530

    code update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import ContentCheckPanel from "./ContentCheckPanel";
 
 function App() {
   return (
-    <div>
+    <div className="container mt-4">
+      <h2>Content Checker</h2>
       <ContentCheckPanel />
     </div>
   );
